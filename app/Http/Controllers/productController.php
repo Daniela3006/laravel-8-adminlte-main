6034afcb6d14867c375b9244b5d471dd5a633074
Daniela <dannapi30@gmail.com>
2022-04-20 10:41:33 +0300

    commit

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -9,59 +9,57 @@ class productController extends Controller
 {
     public function main()
     {
-$products=Product::all();
-
-
-        return view('product.index', compact('products'));
+        //$products = Product::all();
+$category= Category::find(1);
+dd($category->products);
+      //  return view('product.index', compact('products'));
     }
 
 
-public function create()
-{
-return view('product.create');
-
-}
-
-
-public function store()
-{
-    $data= request()->validate([
-        'title'=>'string',
-        'description'=>'string',
-        'price'=>'integer',
-    ]);
-   $product= Product:: create($data);
-   return redirect()->route('product.main');
-}
+    public function create()
+    {
+        return view('product.create');
+    }
 
 
+    public function store()
+    {
+        $data = request()->validate([
+            'title' => 'string',
+            'description' => 'string',
+            'price' => 'integer',
+        ]);
+        $product = Product::create($data);
+        return redirect()->route('product.main');
+    }
 
-public function show(Product $product)
-{
-
-return view('product.show',compact('product'));
-}
 
 
-public function edit(Product $product){
-    return view('product.edit', compact('product'));
-}
+    public function show(Product $product)
+    {
 
-public function update(Product $product){
-    $data= request()->validate([
-        'title'=>'string',
-        'description'=>'string',
-        'price'=>'integer',]);
+        return view('product.show', compact('product'));
+    }
+
+
+    public function edit(Product $product)
+    {
+        return view('product.edit', compact('product'));
+    }
+
+    public function update(Product $product)
+    {
+        $data = request()->validate([
+            'title' => 'string',
+            'description' => 'string',
+            'price' => 'integer',
+        ]);
         $product->update($data);
-        return redirect()->route('product.show',$product->id);
-}
-public function destroy(Product $product)
-{
-$product-> delete();
-return redirect(route('product.main'));
-}
-
-
-
-
+        return redirect()->route('product.show', $product->id);
+    }
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect(route('product.main'));
+    }
 }
